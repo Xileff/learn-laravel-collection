ca7bc9b44aa0442ef97c33af7eb200d49ee51277
Felix <felixsavero78@gmail.com>
2024-06-28 14:02:20 +0700

    feat: random

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -399,4 +399,11 @@ class CollectionTest extends TestCase
         });
         $this->assertEquals(4, $result);
     }
+
+    public function testRandom()
+    {
+        $collection = collect([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        $result = $collection->random();
+        $this->assertTrue(in_array($result, [1, 2, 3, 4, 5, 6, 7, 8, 9]));
+    }
 }
