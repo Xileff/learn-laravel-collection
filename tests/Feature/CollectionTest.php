d03658f271ad28de3079ccc194389201ed94e4d0
Felix <87980522+Xileff@users.noreply.github.com>
2024-06-28 09:35:55 +0700

    feat: collection manipulation

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -6,14 +6,12 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-use function PHPUnit\Framework\assertEquals;
-
 class CollectionTest extends TestCase
 {
     public function testCreateCollection()
     {
         $collection = collect([1, 2, 3]);
-        assertEquals([1, 2, 3], $collection->all());
+        $this->assertEquals([1, 2, 3], $collection->all());
     }
 
     public function testForEach()
@@ -21,7 +19,18 @@ class CollectionTest extends TestCase
         $collection = collect([1, 2, 3, 4, 5, 6, 7, 8, 9]);
         foreach ($collection as $key => $value) {
             // key itu index nya
-            self::assertEquals($key + 1, $value);
+            $this->assertEquals($key + 1, $value);
         }
+    }
+
+    public function testCrud()
+    {
+        $collection = collect([]);
+        $collection->push(1, 2, 3);
+        $this->assertEquals([1, 2, 3], $collection->all());
+
+        $last = $collection->pop();
+        $this->assertEquals(3, $last);
+        $this->assertEquals([1, 2], $collection->all());
     }
 }
